12c652e423a9e20dbbf5e5ada7a749ead8d7cfe4
salif437 <tsalif190@gmail.com>
2024-08-28 15:01:54 +0000

    ok

--- projet_1.py
+++ projet_1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 from numerize.numerize import numerize
 import time
 import os
